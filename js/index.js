8ed45d2bdc7b5908859731ee7da27a95fcdec515
choizinyoung <110873950+choizinyoung@users.noreply.github.com>
2022-08-11 18:19:52 +0900

    gnb 수정

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-$(function () {
-    var firstmenu = $("nav>ul>li"),
-    headerNav = $(".header_nav"),
-    headerNavHeight=headerNav.height();
+var mainmenu = $("nav>ul>li"), //원뎁스 li 할당
+  subMenu = mainmenu.find("ul"), //투뎁스 ul 할당
+  header = $(".header_nav"), //.header_nav할당
+  headerHeight = header.height(), //header높이 할당
+  subMenuHeight = subMenu.height(), //subMenu 높이할당
+  totalHeaderHeight = headerHeight + subMenuHeight + 200;
 
-    firstmenu.mouseenter(function(){
-        firstmenu.find("ul").hide();
-        $(this).find("ul").show();
-        var submenuHeight = $(this).find("ul").height();
-        headerNav.stop().animate({height:submenuHeight+headerNavHeight},1000);
-    });
+mainmenu.mouseenter(function () {
+  header
+    .addClass("active")
+    .stop()
+    .animate({ height: totalHeaderHeight + "px" }, 300);
+});
+mainmenu.mouseleave(function () {
+  header
+    .removeClass("active")
+    .stop()
+    .animate({ height: headerHeight + "px" }, 300);
 });
