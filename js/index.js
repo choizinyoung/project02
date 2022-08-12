62e61d17f4aefa546fdecfed73dc0b64477c71ac
choizinyoung <110873950+choizinyoung@users.noreply.github.com>
2022-08-12 18:14:18 +0900

    gnb디자인수정 배너디자인수정

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//gnbmenu
 var mainmenu = $("nav>ul>li"), //원뎁스 li 할당
   subMenu = mainmenu.find("ul"), //투뎁스 ul 할당
   header = $(".header_nav"), //.header_nav할당
@@ -17,3 +18,45 @@ mainmenu.mouseleave(function () {
     .stop()
     .animate({ height: headerHeight + "px" }, 300);
 });
+
+
+
+
+//swiper
+var swiper = new Swiper(".Swiper1", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper2 = new Swiper(".Swiper2", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper3 = new Swiper(".Swiper3", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
